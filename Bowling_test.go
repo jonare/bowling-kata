447ad54a946fb8b93960ef06ae01910e576397ce
Jon Are Rakvaag <jon.are.rakvaag@sparebank1.no>
2019-04-04 10:28:28 +0200

    Remove redundant declarations

--- Bowling_test.go
+++ Bowling_test.go
@@ -5,24 +5,24 @@ import "testing"
 func TestGame90(t *testing.T) {
 	game := Game{
 		frames: [12]Frame{
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{9, 0},
-			Frame{0, 0},
-			Frame{0, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{9, 0},
+			{0, 0},
+			{0, 0},
 		},
 	}
 	got := BowlingGame(game)
 	want := 90
 
-	if (got != want) {
+	if got != want {
 		t.Errorf("got '%d' want '%d'", got, want)
 	}
 }
@@ -30,24 +30,24 @@ func TestGame90(t *testing.T) {
 func TestGame300(t *testing.T) {
 	game := Game{
 		 frames: [12]Frame{
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
-			Frame{10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
+			 {10, 0},
 		},
 	}
 	got := BowlingGame(game)
 	want := 300
 
-	if (got != want) {
+	if got != want {
 		t.Errorf("got '%d' want '%d'", got, want)
 	}
 }
@@ -55,24 +55,24 @@ func TestGame300(t *testing.T) {
 func TestGame150(t *testing.T) {
 	game := Game{
 		frames: [12]Frame{
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{5, 5},
-			Frame{0, 0},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{5, 5},
+			{0, 0},
 		},
 	}
 	got := BowlingGame(game)
 	want := 150
 
-	if (got != want) {
+	if got != want {
 		t.Errorf("got '%d' want '%d'", got, want)
 	}
 }
@@ -80,24 +80,24 @@ func TestGame150(t *testing.T) {
 	func TestGame123(t *testing.T) {
 		game := Game{
 			frames: [12]Frame{
-				Frame{10, 0}, 	//20
-				Frame{5, 5},		//40
-				Frame{10, 0},		//70
-				Frame{10, 0},		//95
-				Frame{10, 0},		//114
-				Frame{5, 4},		//123
-				Frame{0, 0},
-				Frame{0, 0},
-				Frame{0, 0},
-				Frame{0, 0},
-				Frame{0, 0},
-				Frame{0, 0},
+				{10, 0}, //20
+				{5, 5},  //40
+				{10, 0}, //70
+				{10, 0}, //95
+				{10, 0}, //114
+				{5, 4},  //123
+				{0, 0},
+				{0, 0},
+				{0, 0},
+				{0, 0},
+				{0, 0},
+				{0, 0},
 			},
 		}
 		got := BowlingGame(game)
 		want := 123
 
-		if (got != want) {
+		if got != want {
 			t.Errorf("got '%d' want '%d'", got, want)
 		}
 }
